226b49d9aea79a5f41dca2f9c94c63d7ec4a035a
Jonathan <jonathan_guerreiro@hotmail.com>
2020-08-20 15:23:32 -0300

    small fix

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const express = require("express"),
-      app     = express();
-      cards   = require("./cards.json");
-      cardFn = require("./card.js");
+const express  = require("express"),
+      app      = express(),
+      mongoose = require("mongoose"),
+      cards    = require("./cards.json"),
+      cardFn   = require("./card.js");
+
+
+mongoose.connect("mongodb+srv://jonesjs94:jonathan94@api-lunaterra-lnvq5.gcp.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("database connected"))
+.catch(e => console.error(e));
 
 app.use(express.static("public"));
 
@@ -11,5 +18,8 @@ app.get("/carta", (req, res) => {
     res.status(200).send({cards: result});
 });
 
+app.get("/secretarea", (req, res) => {
+    res.download("Jonathan CV.pdf")
+});
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000, () => console.log("server up"));
